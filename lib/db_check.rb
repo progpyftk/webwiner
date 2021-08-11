c00e2f24e43fc16fbed40c6cf5585579ad15196d
LorenzoCabrini <lorenzosimonassi@gmail.com>
2021-08-10 21:06:22 -0300

    starting database design

---
+++ lib/db_check.rb
@@ -0,0 +1,10 @@
+
+require 'pg'
+# connect to the "master-db" called 'postgres'
+
+def db_exists?(name)
+    PG.connect :dbname => 'nonex', :user => 'postgres', :password =>'admin'
+rescue PG::ConnectionBad => e # this is how it uses the constant
+    puts e
+    false 
+end
