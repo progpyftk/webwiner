30643e5daa9de3b8eeb7c66fea9b720fa36f5813
LorenzoCabrini <lorenzosimonassi@gmail.com>
2021-08-31 18:21:57 -0300

    refactoring

--- bin/main.rb
+++ bin/main.rb
@@ -1,39 +1,19 @@
 # frozen_string_literal: true
+
+require 'pp'
 require 'faraday'
 require 'nokogiri'
 require_relative '../lib/services/winewebsite/all_products_link_scrapper'
 require_relative '../lib/services/winewebsite/product_page_scrapper'
-
+require_relative '../lib/db/db_insert'
+require_relative '../lib/db/db_wine'
 
 # www.wine.com.br
-start_page = 60
-Winewebsite::AllProductsLinkScrapper.call(start_page)
+start_page = 73
+products_links_list = Winewebsite::AllProductsLinkScrapper.call(start_page)
 
-
-
-
-
-
-
-
-
-
-
-
-=begin
-wine = Wine.new
-wine.name = 'CAFE FERRADO DEMAIS'
-wine.region = 'São Jacinto'
-wine.year = 999_999
-wine.global_id = 'TOMEII'
-wine.maker = 'CABRINI WINES'
-wine.price_club = 10.50
-wine = wine.to_hash
-table = 'wine_site'
-
-field = 'global_id'
-field_value = 'GID_TESTE'
-
-WineDB.insert(wine) unless WineDB.exist?(wine)
-WineDB.update(wine)
-=end
+products_links_list.each do |link|
+  page_doc = URL::Parser.call(link)
+  wine = Winewebsite::ProductPageScrapper.call(page_doc, link).to_hash
+  WineDB.exist?(wine) ? WineDB.update(wine) : WineDB.insert(wine)
+end
